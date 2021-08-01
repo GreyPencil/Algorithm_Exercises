eff8da6162e2ab276081bc7f2ee124ad75f6a37b
GreyPencil <kangsuze@gmail.com>
2021-07-31 22:35:24 -0400

    medium 2

--- medium/2. Smallest_Difference.js
+++ medium/2. Smallest_Difference.js
@@ -6,7 +6,32 @@ Note that the absolute difference of two integers is the distance between them
 on the real number lne. For example, the absolute difference of -5 and 5 is 10, and the absolute difference of -5 and -1 is 1
 
 you can assume that there will only be one pair of numbers with the smallest difference*/
-
+/* O(nlog(n) + mlog(m)) time |  O(1) space*/
 function smallestDifference(arrayOne, arrayTwo){
-    
+    arrayOne.sort((a,b) => a -b);
+    arrayTwo.sort((a,b) => a -b);
+    let array = [];
+    let target = arrayTwo[arrayTwo.length-1] - arrayOne[0];
+    let smallest = arrayTwo[arrayTwo.length-1] - arrayOne[0];
+    let indexOne = 0;
+    let indexTwo = 0;
+    while (indexOne < arrayOne.length && indexTwo < arrayTwo.length){
+        let firstNum = arrayOne[indexOne];
+        let secondNum = arrayTwo[indexTwo];
+        if(firstNum < secondNum){
+            target = secondNum - firstNum;
+            indexOne++;
+        } else if(firstNum > secondNum){
+            target = firstNum - secondNum;
+            indexTwo++;
+        } else if(firstNum === secondNum){
+            return array = [firstNum,firstNum];
+        }
+        if(smallest > target){
+            smallest = target;
+            array = [firstNum,secondNum]
+        }
+    }
+    return array;
 }
+exports.smallestDifference = smallestDifference;
