9fd42189d2e4f7ab0d1c4c54a55ef681cb2e9237
GreyPencil <kangsuze@gmail.com>
2021-08-06 14:41:06 -0400

    easy 6

--- easy/6.find closest value in BST.js
+++ easy/6.find closest value in BST.js
@@ -7,3 +7,35 @@ if and only if it satisfies the BST property: its value is strictly geater than 
 its value is less than or equal to the values of every node to its right; and its children nodes 
 are either valid BST nodes themselves or Non/null
 */
+// O(logn) time, O(1) space
+function findClosestValueInBst(tree, target) {
+    // Write your code here.
+    return findClosestValueHelper(tree, target, tree.value);
+  }
+
+  function findClosestValueHelper(tree, target, closest){
+     if (tree === null) return closest;
+     if(Math.abs(target - closest) > Math.abs(target-tree.value)){
+         closest = tree.value;
+     } 
+     if(target < tree.value){
+         return findClosestValueHelper(tree.left, target, closest)
+     }
+     if(target > tree.value){
+        return findClosestValueHelper(tree.right, target, closest)
+    } else {
+        return closest;
+    }
+  }
+  // This is the class of the input tree. Do not edit.
+  class BST {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  // Do not edit the line below.
+  exports.findClosestValueInBst = findClosestValueInBst;
+  
