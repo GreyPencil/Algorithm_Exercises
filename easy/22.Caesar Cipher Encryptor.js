39c4389be37fdb89beddc59b015f2d54a805b53d
GreyPencil <kangsuze@gmail.com>
2021-08-15 19:20:55 -0400

    easy 22 1st answer

---
+++ easy/22.Caesar Cipher Encryptor.js
@@ -0,0 +1,25 @@
+/* 
+given a non-empty string of lowercase letters and a non-negative integer representing a key
+write a function that returns a new string obtained by shifting every letter in the input string by k positions in the alphabet, where k is the key.
+
+Note that letters should "wrap" around the alphabet; in other words, the letter z shifted by one returns the letter a
+*/
+
+//sample input : string = "xyz" key = 2;
+// sample output: "zab"
+// O(n) time and O(n) space
+
+function caesarCipherEncryptor(string, key) {
+  // Write your code here.
+  const newLetters = [];
+  const newKey = key%26;
+  for (const letter of string) {
+      newLetters.push(getNewLetter(letter, newKey));
+  }
+  return newLetters.join('');
+}
+
+function getNewLetter(letter, key){
+    const newLetterCode = letter.charCodeAt() + key;
+    return newLetterCode <= 122 ? String.fromCharCode(newLetterCode) : String.fromCharCode(96+ (newLetterCode %122));
+}
