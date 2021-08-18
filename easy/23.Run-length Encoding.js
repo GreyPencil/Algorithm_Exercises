ddd669b93c6d2597e4b1c5c3ce95532b8eb7c673
GreyPencil <kangsuze@gmail.com>
2021-08-17 23:20:07 -0400

    easy 23

--- easy/23.Run-length Encoding.js
+++ easy/23.Run-length Encoding.js
@@ -5,3 +5,29 @@ max 9
 */
 // O(n) time and O(n) space
 
+function runLengthEncoding(string) {
+    // Write your code here.
+    const encodedStringCharacters = [];
+    let num = 1;
+
+    for (let i = 1; i < string.length; i++){
+        const current = string[i];
+        const pre = string[i-1]
+        if(num === 9 || current !== pre){
+            encodedStringCharacters.push(num.toString());
+            console.log(encodedStringCharacters);
+            console.log(num);
+            encodedStringCharacters.push(string[i-1]);
+            num = 0;
+        }
+        num++;
+    }
+    encodedStringCharacters.push(num.toString());
+    encodedStringCharacters.push(string[string.length-1]);
+
+    return encodedStringCharacters.join('');
+
+  }
+
+  runLengthEncoding("AAAABB");
+  
