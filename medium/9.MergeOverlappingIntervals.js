f33bf507122bf7065172508ea3eb7eb0bdc6874b
GreyPencil <kangsuze@gmail.com>
2021-08-25 20:02:10 -0400

    medium 9

---
+++ medium/9.MergeOverlappingIntervals.js
@@ -0,0 +1,20 @@
+/* 
+non-empty array of arbitrary intervals, merges any overlapping intervals, and returns the new intervals in no particular order.
+
+interval is an array of two integers, with interval[0] as the start of the interval and interval[1] as the end
+
+note that backtoback itnervals aren't considered to be overlapping. For example, [1,5,] and [6,7] aren't overlapping; [1,6] [6,7 are indeed overlapping]
+
+sample input: "intervals": [
+    [1, 2],
+    [3, 5],
+    [4, 7],
+    [6, 8],
+    [9, 10]
+  ]
+  sample output: "intervals": [
+    [1, 2],
+    [3, 8],
+    [9, 10]
+  ]
+*/
