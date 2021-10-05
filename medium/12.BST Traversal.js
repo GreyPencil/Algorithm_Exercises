7773c8b5985e4b5e61a49c69db3c9a68102e3d8b
GreyPencil <kangsuze@gmail.com>
2021-10-04 20:18:14 -0400

    medium 12

---
+++ medium/12.BST Traversal.js
@@ -0,0 +1,25 @@
+/* 
+Write three functions that take in a Binary Search Tree(BST) and an empty array, traverse the BST, add its
+nodes' values to the input array, and return that array. The three functions should traverse the BST using the in-order, pre-order, and post-order
+tree-traversal techniques, respectively.
+Each BST node has an integer value, a left child node, and a right child node. A node is said to be a valid BST node if and only if it satisfies the BST property:
+its value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node
+to its right; and its children nodes are either valid BST nodes themselves or None /null
+*/
+
+function inOrderTraverse(tree, array) {
+    // Write your code here.
+  }
+  
+  function preOrderTraverse(tree, array) {
+    // Write your code here.
+  }
+  
+  function postOrderTraverse(tree, array) {
+    // Write your code here.
+  }
+  
+  // Do not edit the lines below.
+  exports.inOrderTraverse = inOrderTraverse;
+  exports.preOrderTraverse = preOrderTraverse;
+  exports.postOrderTraverse = postOrderTraverse;
