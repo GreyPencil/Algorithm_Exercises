aebd7f71bc7362bf6a7b742b4d97d4f615e98619
GreyPencil <kangsuze@gmail.com>
2021-10-05 23:29:29 -0400

    medium 12

--- medium/12.BST Traversal.js
+++ medium/12.BST Traversal.js
@@ -8,18 +8,37 @@ to its right; and its children nodes are either valid BST nodes themselves or No
 */
 
 function inOrderTraverse(tree, array) {
-    // Write your code here.
-  }
-  
-  function preOrderTraverse(tree, array) {
-    // Write your code here.
-  }
-  
-  function postOrderTraverse(tree, array) {
-    // Write your code here.
-  }
-  
-  // Do not edit the lines below.
-  exports.inOrderTraverse = inOrderTraverse;
-  exports.preOrderTraverse = preOrderTraverse;
-  exports.postOrderTraverse = postOrderTraverse;
+  // Write your code here.
+	if (tree !== null){
+		inOrderTraverse(tree.left, array);
+		array.push(tree.value);
+		inOrderTraverse(tree.right, array);
+	}
+	return array;
+}
+
+function preOrderTraverse(tree, array) {
+  // Write your code here.
+	if (tree !== null){
+		array.push(tree.value);
+		preOrderTraverse(tree.left, array);	
+		preOrderTraverse(tree.right, array);
+	}
+	return array;
+}
+
+function postOrderTraverse(tree, array) {
+  // Write your code here.
+  //先左，再右，再根
+	if(tree !== null){
+		postOrderTraverse(tree.left, array);
+		postOrderTraverse(tree.right, array);
+		array.push(tree.value);
+	}
+	return array;
+}
+
+// Do not edit the lines below.
+exports.inOrderTraverse = inOrderTraverse;
+exports.preOrderTraverse = preOrderTraverse;
+exports.postOrderTraverse = postOrderTraverse;
