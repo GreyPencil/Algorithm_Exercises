65c44fb858296aab6fd51f830c03a05b84d81f84
GreyPencil <kangsuze@gmail.com>
2021-10-11 19:53:28 -0400

    medium 13 first solution

--- medium/13.Min_Height_BST.js
+++ medium/13.Min_Height_BST.js
@@ -19,33 +19,50 @@ Sample output:
 */
 
 function minHeightBst1(array) {
-    // Write your code here.
-    //O(nlogn) time     O(n) space
+  // Write your code here.
+  //O(nlogn) time     O(n) space
+  return constructMinHeightBst(array, null, 0, array.length - 1);
+}
+
+
+function constructMinHeightBst(array, bst, startIdx, endIdx) {
+  if (endIdx < startIdx) return;
+  const midIdx = Math.floor((startIdx + endIdx) / 2);
+  const valueToAdd = array[midIdx];
+  if (bst === null) {
+    bst = new BST(valueToAdd);
+  } else {
+    bst.insert(valueToAdd);
   }
-  
-  class BST {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-    }
-  
-    insert(value) {
-      if (value < this.value) {
-        if (this.left === null) {
-          this.left = new BST(value);
-        } else {
-          this.left.insert(value);
-        }
+  constructMinHeightBst(array, bst, startIdx, midIdx - 1);
+  constructMinHeightBst(array, bst, midIdx + 1, endIdx);
+  return bst;
+}
+
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
       } else {
-        if (this.right === null) {
-          this.right = new BST(value);
-        } else {
-          this.right.insert(value);
-        }
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
       }
     }
   }
-  
-  // Do not edit the line below.
-  exports.minHeightBst = minHeightBst;
+}
+
+// Do not edit the line below.
+exports.minHeightBst = minHeightBst;
