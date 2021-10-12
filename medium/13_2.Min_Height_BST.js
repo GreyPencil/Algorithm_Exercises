36dfc9ac121e0a7d950b11d8ffbb67c63951a064
GreyPencil <kangsuze@gmail.com>
2021-10-11 20:05:12 -0400

    medium 13 second solution

--- medium/13_2.Min_Height_BST.js
+++ medium/13_2.Min_Height_BST.js
@@ -18,7 +18,7 @@ Sample output:
         7          22
 */
 
-function minHeightBst2(array) {
+function minHeightBst(array) {
     // Write your code here.
     //O(n) time     O(n) space
     return constructMinHeightBst(array, null, 0, array.length - 1);
@@ -28,11 +28,11 @@ function minHeightBst2(array) {
   function constructMinHeightBst(array, bst, startIdx, endIdx) {
     if (endIdx < startIdx) return;
     const midIdx = Math.floor((startIdx + endIdx) / 2);
-    const valueToAdd = array[midIdx];
+    const newBstNode = new BST(array[midIdx]);
     if (bst === null) {
-      bst = new BST(valueToAdd);
+      bst = newBstNode;
     } else {
-      if (array[midInd]< bst.value){
+      if (array[midIdx]< bst.value){
           bst.left = newBstNode;
           bst = bst.left;
       }else{
@@ -43,32 +43,31 @@ function minHeightBst2(array) {
     constructMinHeightBst(array, bst, startIdx, midIdx - 1);
     constructMinHeightBst(array, bst, midIdx + 1, endIdx);
     return bst;
+}
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-  
-  
-  class BST {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-    }
-  
-    insert(value) {
-      if (value < this.value) {
-        if (this.left === null) {
-          this.left = new BST(value);
-        } else {
-          this.left.insert(value);
-        }
+
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
       } else {
-        if (this.right === null) {
-          this.right = new BST(value);
-        } else {
-          this.right.insert(value);
-        }
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
       }
     }
   }
-  
-  // Do not edit the line below.
-  exports.minHeightBst = minHeightBst;
+}
+
+// Do not edit the line below.
+exports.minHeightBst = minHeightBst;
