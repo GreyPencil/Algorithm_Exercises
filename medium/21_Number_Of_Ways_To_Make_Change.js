7cdeaa6569a1ca3fee0496eb640f4b74eec5aad1
GreyPencil <kangsuze@gmail.com>
2021-10-15 23:40:23 -0400

    medium 21

---
+++ medium/21_Number_Of_Ways_To_Make_Change.js
@@ -0,0 +1,6 @@
+/* 
+Given an array of distinct positive integers representing coin denominations and a single non-negative
+integer n representing a targe amount of money, write a function that returns the number of ways to make 
+change for that target amount using the given coin denominations.
+Note that an unlimited amount of coins is at your disposal.
+*/
