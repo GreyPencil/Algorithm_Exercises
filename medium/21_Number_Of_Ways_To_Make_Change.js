5f37a539f3b8184f9ed2a503d8cc574969e5c695
GreyPencil <kangsuze@gmail.com>
2021-10-16 00:01:27 -0400

    medium 21

--- medium/21_Number_Of_Ways_To_Make_Change.js
+++ medium/21_Number_Of_Ways_To_Make_Change.js
@@ -4,3 +4,24 @@ integer n representing a targe amount of money, write a function that returns th
 change for that target amount using the given coin denominations.
 Note that an unlimited amount of coins is at your disposal.
 */
+
+function numberOfWaysToMakeChange(n, denoms) {
+    // Write your code here.
+    // O(N*d) time | O(N) space
+    const ways = new Array(n+1).fill(0);
+    ways[0] = 1;
+    for(let denom of denoms) {
+        for (let amount = 1; amount <= n; amount++){
+            if(denom <= amount){
+                ways[amount] += ways[amount - denom];
+            }
+        }
+
+    }
+    return ways[n];
+
+
+  }
+  
+  // Do not edit the line below.
+  exports.numberOfWaysToMakeChange = numberOfWaysToMakeChange;
