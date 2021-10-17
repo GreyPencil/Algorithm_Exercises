2ae48ac389f1dc01c80cc94084c646ccc7b3f999
GreyPencil <kangsuze@gmail.com>
2021-10-16 22:13:38 -0400

    medium 24 solution 2

--- medium/24_number_OF_Ways_To_Traverse_Graph.js
+++ medium/24_number_OF_Ways_To_Traverse_Graph.js
@@ -20,6 +20,31 @@ function numberOfWaysToTraverseGraph(width, height) {
 
     return numberOfWaysToTraverseGraph(width -1, height) + numberOfWaysToTraverseGraph(width, height-1);
   }
+
+  function numberOfWaysToTraverseGraph2(width, height) {
+    // Write your code here.
+    // O(n*m) time  || O(n*m) space
+    const numberOfWays =[]
+    for(let i=0; i<height +1; i++){
+        numberOfWays.push([]);
+        for (let j =0; j< width +1;j++){
+            numberOfWays[i].push(0)
+        }
+    }
+
+    for (let widthIdx =1; widthIdx < width +1; widthIdx++){
+        for (let heightIdx =1; heightIdx <height+1; heightIdx++){
+            if (widthIdx === 1 || heightIdx ===1){
+                numberOfWays[heightIdx][widthIdx] =1;
+            } else {
+                const waysLeft = numberOfWays[heightIdx][widthIdx -1];
+                const wayUp = numberOfWays[heightIdx-1][widthIdx]
+                numberOfWays[heightIdx][widthIdx] = waysLeft + wayUp;
+            }
+        }
+    }
+    return numberOfWays[height][width]
+  }
   
   // Do not edit the line below.
   exports.numberOfWaysToTraverseGraph = numberOfWaysToTraverseGraph;
