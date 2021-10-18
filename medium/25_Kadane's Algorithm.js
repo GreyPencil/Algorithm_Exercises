fceaaf7d438cc890ce818aaf4a68b2cde3c00516
GreyPencil <kangsuze@gmail.com>
2021-10-18 00:01:54 -0400

    medium 25 updated

---
+++ medium/25_Kadane's Algorithm.js
@@ -0,0 +1,8 @@
+/* 
+Write a function that takes in a non-empty array of integers and returns the max sum 
+that can be obtained by summing up all of the integers in a non-empty subarray of the input array. 
+A subarray must only contain adjacent numbers (numbers next to each other in the input array).
+
+Sample Input: array = [3,5,-9,1,3,-2,3,4,7,2,-9,6,3,1,-5,4]
+Sample output: 19     [1,3,-2,3,4,7,2,-9,6,3,1]
+*/
