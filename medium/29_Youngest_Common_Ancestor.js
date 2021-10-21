49039e48e422910b8f207c2bf5a144f74400a4e4
GreyPencil <kangsuze@gmail.com>
2021-10-20 21:47:10 -0400

    medium 29

--- medium/29_Youngest_Common_Ancestor.js
+++ medium/29_Youngest_Common_Ancestor.js
@@ -23,3 +23,49 @@ descendanteTwo = node I
 Sample Out put: B
  */
 
+// This is an input class. Do not edit.
+class AncestralTree {
+    constructor(name) {
+      this.name = name;
+      this.ancestor = null;
+    }
+  }
+  
+//   O(D) time      O(1) space
+  function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {
+    // Write your code here.
+    const depthOne = getDescendantDepth(descendantOne, topAncestor)
+    const depthTwo = getDescendantDepth(descendantTwo, topAncestor)
+    if (depthOne > depthTwo){
+        return backtrackAncestralTree(descendantOne, descendantTwo, depthOne - depthTwo)
+    } else {
+        return backtrackAncestralTree(descendantTwo, descendantOne, depthTwo - depthOne)
+    }
+  }
+
+  function getDescendantDepth(descendant, topAncestor) {
+      let depth = 0;
+      while (descendant !== topAncestor){
+          depth++
+          descendant = descendant.ancestor
+      }
+      return depth;
+  }
+
+  function backtrackAncestralTree(lowerDescendant, higherDescendant, diff){
+      while (diff > 0) {
+          lowerDescendant = lowerDescendant.ancestor
+          diff--;
+      }
+      while (lowerDescendant !== higherDescendant){
+          lowerDescendant = lowerDescendant.ancestor
+          higherDescendant = higherDescendant.ancestor
+      }
+      return lowerDescendant;
+  }
+
+  
+  // Do not edit the lines below.
+  exports.AncestralTree = AncestralTree;
+  exports.getYoungestCommonAncestor = getYoungestCommonAncestor;
+
