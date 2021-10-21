56cf3c1506f79b6c269722cd2e73cb34ce3d87ad
GreyPencil <kangsuze@gmail.com>
2021-10-20 21:35:25 -0400

    medium 29

---
+++ medium/29_Youngest_Common_Ancestor.js
@@ -0,0 +1,25 @@
+
+/*
+You're given three inputs, all of which are instances of an AncestralTree class that have an ancestor property pointing to their youngest ancestor.
+The first input is the top ancestor in an ancestral tree (i.e., the only instance that has no ancestor-its ancestor property points to None/null) 
+, and the other two inputs are descendants in the ancestral tree.
+
+Write a function that returns the youngest common ancestor to the two descendants.
+
+Note that a descendant is considered its own ancestor. So in the simple ancestral tree below, the youngest common ancestor to nodes A and B is node A.
+
+Sample Input: graph =                               A
+                                                /     \
+                                             B         C
+                                           /   \       /  \ 
+                                         D      E     F    G
+                                        / \     \
+                                       H   I    J
+
+topAncestor = node A
+descendantOne = node E
+descendanteTwo = node I                                            
+
+Sample Out put: B
+ */
+
