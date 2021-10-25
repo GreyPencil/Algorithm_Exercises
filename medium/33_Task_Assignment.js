6b3b24ec9dbb10e0dac0ec4581825e71e85ba66e
GreyPencil <kangsuze@gmail.com>
2021-10-24 23:56:07 -0400

    medium 33

---
+++ medium/33_Task_Assignment.js
@@ -0,0 +1,9 @@
+/* 
+You're given an integer k representing anumber of workers and an array of positive integers representing durations of tasks that
+must be completed by the workers. Specifically, each worker must complete two unique tasks and can only work on one task at a time.
+the number of tasks will always be equal to 2k such that each worker always has exactly two tasks to complete. All tasks are
+independent of one another and can be completed in any oreder. Workers will complete their assigned tasks in parallel, and the time
+taken to complete all tasks will be equal to the time taken to complete the longest pair of tasks 
+
+Write a function that returns the optimal assignment of tasks to each worker such taht the tasks are 
+*/
