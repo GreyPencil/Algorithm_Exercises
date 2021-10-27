6dbd05b53d06490b6f438bf31937fb670eb5e11e
GreyPencil <kangsuze@gmail.com>
2021-10-26 22:10:58 -0400

    medium 36

---
+++ medium/36_Linked_List_Construction.js
@@ -0,0 +1,9 @@
+/* 
+Write a DoublyLinkedList class that has a head and a tail, both of which point to either a linked list Node or None/null. The class should support:
+1. setting the head and tail of the linked list.
+2. Inserting nodes before and after other nodes as well as at given positions(the position of the head node is 1)
+3. Removing given nodes and removing nodes with given values.
+4. searching for nodes with given values.
+
+Note that
+*/
