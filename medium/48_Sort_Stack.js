d10494829ac0b1a4302ac52fb7bd2d333a5f9bc4
GreyPencil <kangsuze@gmail.com>
2021-11-14 00:19:09 -0500

    medium 48

---
+++ medium/48_Sort_Stack.js
@@ -0,0 +1,12 @@
+/* 
+Write a function that takes in an array of integers representing a stack, recursively sorts the
+stack in place (i.e., doesn't create a brand new array) and returns it.
+
+The array must be treated as a stack, with the end of the array as the top of the stack.
+Therefore, you're only allowed to
+- Pop elements from the top of the stack by removing elements from the end of the array
+using the built-in .pop() method in your programming language of choice.
+- Push elements to the top of the stack by appending elements to the end of the array
+using the built-in .append() method in your programming language of choice.
+- Peek at the element on top of the stack by accessing the last element in the array.
+*/
