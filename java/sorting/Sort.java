52b8ccbd83647ced3b6515df907bffdd9e15c51b
suzek-klf <suzeklf@gmail.com>
2024-10-21 21:36:14 -0400

    bubble sort

--- java/sorting/Sort.java
+++ java/sorting/Sort.java
@@ -8,11 +8,11 @@ public class Sort {
         int size = intArray.length;
         // int[] newArray = new int[size];
 
-        for(int i = 1; i < size; i++) {
+        for (int i = 1; i < size; i++) {
             int key = intArray[i];
             int j = i - 1;
 
-            while(j >= 0 && key < intArray[j]){
+            while (j >= 0 && key < intArray[j]) {
                 intArray[j + 1] = intArray[j];
                 j--;
             }
@@ -25,32 +25,32 @@ public class Sort {
 
     }
 
-    public void insertionSortDesc(int[] intArray){
+    public void insertionSortDesc(int[] intArray) {
         int size = intArray.length;
-        
-        for(int i = 1; i < size; i++){
-            int j = i -1;
+
+        for (int i = 1; i < size; i++) {
+            int j = i - 1;
             int key = intArray[i];
 
-            while(j>=0 && key > intArray[j]){
-                intArray[j+1] = intArray[j];
+            while (j >= 0 && key > intArray[j]) {
+                intArray[j + 1] = intArray[j];
                 j--;
             }
-            intArray[j+1] = key;
+            intArray[j + 1] = key;
         }
 
-        for(int num: intArray) {
+        for (int num : intArray) {
             System.out.println(num + " ");
         }
     }
 
-    public void selectionSort(int[] intArray){
+    public void selectionSort(int[] intArray) {
         int size = intArray.length;
-        for(int i = 0; i< size-1; i++){
+        for (int i = 0; i < size - 1; i++) {
             int min_index = i;
 
-            for(int j = i+1; j < size; j++ ){
-                if(intArray[min_index] > intArray[j]){
+            for (int j = i + 1; j < size; j++) {
+                if (intArray[min_index] > intArray[j]) {
                     min_index = j;
                 }
             }
@@ -59,18 +59,41 @@ public class Sort {
             intArray[min_index] = temp;
         }
 
-        for(int num: intArray) {
+        for (int num : intArray) {
+            System.out.println(num + " ");
+        }
+    }
+
+    public void bubbleSort(int[] arr) {
+        int size = arr.length;
+
+        for (int i = size; i > 0; i--) {
+            boolean swapped = false;
+            for(int j = 0; j<i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            System.out.println("swapped " + swapped);
+            if(!swapped) break;
+        }
+
+        for (int num : arr) {
             System.out.println(num + " ");
         }
     }
 
     public static void main(String[] args) {
-        int[] intArray = new int[] {300, -20, -15, 5, 7, 1, 3, 2, 4, 10, 124 };
+        int[] intArray = new int[] { 300, -20, -15, 5, 7, 1, 3, 2, 4, 10, 124 };
 
         Sort sort = new Sort();
         // sort.insertionSort(intArray);
         // sort.insertionSortDesc(intArray);
-        sort.selectionSort(intArray);
+        // sort.selectionSort(intArray);
+        sort.bubbleSort(intArray);
 
     }
 }
